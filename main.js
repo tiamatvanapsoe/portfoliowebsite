b875dd4c1aaea10ffe07828dd95e25f6cd340fc7
Tiamat van Apsoe <tiamatvanapsoe@gmail.com>
2022-06-13 16:14:16 +0200

    per icoon zelfde blok code herhaald

--- main.js
+++ main.js
@@ -10,3 +10,72 @@ icoon1.onmouseover = verschijn1;
 icoon1.onmouseout = verdwijn1;
 
 
+let icoon2 = document.getElementById('icoon2');
+let tekstBijIcoon2 = document.getElementById('tekst-bij-icoon2')
+let verschijn2 = function(){
+    tekstBijIcoon2.style.display = 'block'
+}
+let verdwijn2 = function(){
+    tekstBijIcoon2.style.display = 'none'
+}
+icoon2.onmouseover = verschijn2;
+icoon2.onmouseout = verdwijn2;
+
+
+let icoon3 = document.getElementById('icoon3');
+let tekstBijIcoon3 = document.getElementById('tekst-bij-icoon3')
+let verschijn3 = function(){
+    tekstBijIcoon3.style.display = 'block'
+}
+let verdwijn3 = function(){
+    tekstBijIcoon3.style.display = 'none'
+}
+icoon3.onmouseover = verschijn3;
+icoon3.onmouseout = verdwijn3;
+
+
+let icoon4 = document.getElementById('icoon4');
+let tekstBijIcoon4 = document.getElementById('tekst-bij-icoon4')
+let verschijn4 = function(){
+    tekstBijIcoon4.style.display = 'block'
+}
+let verdwijn4 = function(){
+    tekstBijIcoon4.style.display = 'none'
+}
+icoon4.onmouseover = verschijn4;
+icoon4.onmouseout = verdwijn4;
+
+
+let icoon5 = document.getElementById('icoon5');
+let tekstBijIcoon5 = document.getElementById('tekst-bij-icoon5')
+let verschijn5 = function(){
+    tekstBijIcoon5.style.display = 'block'
+}
+let verdwijn5 = function(){
+    tekstBijIcoon5.style.display = 'none'
+}
+icoon5.onmouseover = verschijn5;
+icoon5.onmouseout = verdwijn5;
+
+
+let icoon6 = document.getElementById('icoon6');
+let tekstBijIcoon6 = document.getElementById('tekst-bij-icoon6')
+let verschijn6 = function(){
+    tekstBijIcoon6.style.display = 'block'
+}
+let verdwijn6 = function(){
+    tekstBijIcoon6.style.display = 'none'
+}
+icoon6.onmouseover = verschijn6;
+icoon6.onmouseout = verdwijn6;
+
+let icoon7 = document.getElementById('icoon7');
+let tekstBijIcoon7 = document.getElementById('tekst-bij-icoon7')
+let verschijn7 = function(){
+    tekstBijIcoon7.style.display = 'block'
+}
+let verdwijn7 = function(){
+    tekstBijIcoon7.style.display = 'none'
+}
+icoon7.onmouseover = verschijn7;
+icoon7.onmouseout = verdwijn7;
