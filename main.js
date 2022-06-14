179440ffdfeda0007aedd05a621a466cd2d53257
Tiamat van Apsoe <tiamatvanapsoe@gmail.com>
2022-06-14 08:52:21 +0200

    werkt maar combi transitie en js

--- main.js
+++ main.js
@@ -1,81 +1,113 @@
 let icoon1 = document.getElementById('icoon1');
-let tekstBijIcoon1 = document.getElementById('tekst-bij-icoon1')
-let verschijn1 = function(){
-    tekstBijIcoon1.style.display = 'block'
-}
-let verdwijn1 = function(){
-    tekstBijIcoon1.style.display = 'none'
-}
-icoon1.onmouseover = verschijn1;
-icoon1.onmouseout = verdwijn1;
-
+let tekstBijIcoon1 = document.getElementById('tekst-bij-icoon1');
+let topIcoon1 = document.getElementById('top-icoon1');
 
 let icoon2 = document.getElementById('icoon2');
-let tekstBijIcoon2 = document.getElementById('tekst-bij-icoon2')
-let verschijn2 = function(){
-    tekstBijIcoon2.style.display = 'block'
-}
-let verdwijn2 = function(){
-    tekstBijIcoon2.style.display = 'none'
-}
-icoon2.onmouseover = verschijn2;
-icoon2.onmouseout = verdwijn2;
-
+let tekstBijIcoon2 = document.getElementById('tekst-bij-icoon2');
 
 let icoon3 = document.getElementById('icoon3');
-let tekstBijIcoon3 = document.getElementById('tekst-bij-icoon3')
-let verschijn3 = function(){
-    tekstBijIcoon3.style.display = 'block'
-}
-let verdwijn3 = function(){
-    tekstBijIcoon3.style.display = 'none'
-}
-icoon3.onmouseover = verschijn3;
-icoon3.onmouseout = verdwijn3;
-
+let tekstBijIcoon3 = document.getElementById('tekst-bij-icoon3');
 
 let icoon4 = document.getElementById('icoon4');
-let tekstBijIcoon4 = document.getElementById('tekst-bij-icoon4')
-let verschijn4 = function(){
-    tekstBijIcoon4.style.display = 'block'
-}
-let verdwijn4 = function(){
-    tekstBijIcoon4.style.display = 'none'
-}
-icoon4.onmouseover = verschijn4;
-icoon4.onmouseout = verdwijn4;
-
+let tekstBijIcoon4 = document.getElementById('tekst-bij-icoon4');
 
 let icoon5 = document.getElementById('icoon5');
-let tekstBijIcoon5 = document.getElementById('tekst-bij-icoon5')
-let verschijn5 = function(){
-    tekstBijIcoon5.style.display = 'block'
-}
-let verdwijn5 = function(){
-    tekstBijIcoon5.style.display = 'none'
-}
-icoon5.onmouseover = verschijn5;
-icoon5.onmouseout = verdwijn5;
-
+let tekstBijIcoon5 = document.getElementById('tekst-bij-icoon5');
 
 let icoon6 = document.getElementById('icoon6');
-let tekstBijIcoon6 = document.getElementById('tekst-bij-icoon6')
-let verschijn6 = function(){
-    tekstBijIcoon6.style.display = 'block'
-}
-let verdwijn6 = function(){
-    tekstBijIcoon6.style.display = 'none'
-}
-icoon6.onmouseover = verschijn6;
-icoon6.onmouseout = verdwijn6;
+let tekstBijIcoon6 = document.getElementById('tekst-bij-icoon6');
 
 let icoon7 = document.getElementById('icoon7');
-let tekstBijIcoon7 = document.getElementById('tekst-bij-icoon7')
-let verschijn7 = function(){
-    tekstBijIcoon7.style.display = 'block'
+let tekstBijIcoon7 = document.getElementById('tekst-bij-icoon7');
+
+let hoverOverIcoon1 = function(){
+    tekstBijIcoon1.style.display = 'block';
+    tekstBijIcoon2.style.display = 'none';
+    tekstBijIcoon3.style.display = 'none';
+    tekstBijIcoon4.style.display = 'none';
+    tekstBijIcoon5.style.display = 'none';
+    tekstBijIcoon6.style.display = 'none';
+    tekstBijIcoon7.style.display = 'none';
+    topIcoon1.style.display = 'block';
 }
-let verdwijn7 = function(){
-    tekstBijIcoon7.style.display = 'none'
+icoon1.onmouseover = hoverOverIcoon1;
+
+
+
+
+let hoverOverIcoon2 = function(){
+    tekstBijIcoon1.style.display = 'none';
+    tekstBijIcoon2.style.display = 'block';
+    tekstBijIcoon3.style.display = 'none';
+    tekstBijIcoon4.style.display = 'none';
+    tekstBijIcoon5.style.display = 'none';
+    tekstBijIcoon6.style.display = 'none';
+    tekstBijIcoon7.style.display = 'none';
+    topIcoon1.style.display = 'none';
 }
-icoon7.onmouseover = verschijn7;
-icoon7.onmouseout = verdwijn7;
+icoon2.onmouseover = hoverOverIcoon2;
+
+let hoverOverIcoon3 = function(){
+    tekstBijIcoon1.style.display = 'none';
+    tekstBijIcoon2.style.display = 'none';
+    tekstBijIcoon3.style.display = 'block';
+    tekstBijIcoon4.style.display = 'none';
+    tekstBijIcoon5.style.display = 'none';
+    tekstBijIcoon6.style.display = 'none';
+    tekstBijIcoon7.style.display = 'none';
+    topIcoon1.style.display = 'none';
+}
+icoon3.onmouseover = hoverOverIcoon3;
+
+let hoverOverIcoon4 = function(){
+    tekstBijIcoon1.style.display = 'none';
+    tekstBijIcoon2.style.display = 'none';
+    tekstBijIcoon3.style.display = 'none';
+    tekstBijIcoon4.style.display = 'block';
+    tekstBijIcoon5.style.display = 'none';
+    tekstBijIcoon6.style.display = 'none';
+    tekstBijIcoon7.style.display = 'none';
+    topIcoon1.style.display = 'none';
+}
+icoon4.onmouseover = hoverOverIcoon4;
+
+let hoverOverIcoon5 = function(){
+    tekstBijIcoon1.style.display = 'none';
+    tekstBijIcoon2.style.display = 'none';
+    tekstBijIcoon3.style.display = 'none';
+    tekstBijIcoon4.style.display = 'none';
+    tekstBijIcoon5.style.display = 'block';
+    tekstBijIcoon6.style.display = 'none';
+    tekstBijIcoon7.style.display = 'none';
+    topIcoon1.style.display = 'none';
+}
+icoon5.onmouseover = hoverOverIcoon5;
+
+let hoverOverIcoon6 = function(){
+    tekstBijIcoon1.style.display = 'none';
+    tekstBijIcoon2.style.display = 'none';
+    tekstBijIcoon3.style.display = 'none';
+    tekstBijIcoon4.style.display = 'none';
+    tekstBijIcoon5.style.display = 'none';
+    tekstBijIcoon6.style.display = 'block';
+    tekstBijIcoon7.style.display = 'none';
+    topIcoon1.style.display = 'none';
+}
+icoon6.onmouseover = hoverOverIcoon6;
+
+let hoverOverIcoon7 = function(){
+    tekstBijIcoon1.style.display = 'none';
+    tekstBijIcoon2.style.display = 'none';
+    tekstBijIcoon3.style.display = 'none';
+    tekstBijIcoon4.style.display = 'none';
+    tekstBijIcoon5.style.display = 'none';
+    tekstBijIcoon6.style.display = 'none';
+    tekstBijIcoon7.style.display = 'block';
+    topIcoon1.style.display = 'none';
+}
+icoon7.onmouseover = hoverOverIcoon7;
+
+
+
+
+ 
