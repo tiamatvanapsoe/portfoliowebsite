235fffdbc1840f79fe6adff17c2381d16bc54e1d
Tiamat van Apsoe <tiamatvanapsoe@gmail.com>
2022-06-16 08:43:17 +0200

    opgekuiste versie tem projecten

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-let icoon1 = document.getElementById('icoon1');
-let tekstBijIcoon1 = document.getElementById('tekst-bij-icoon1')
-let verschijn1 = function(){
-    tekstBijIcoon1.style.display = 'block'
-}
-let verdwijn1 = function(){
-    tekstBijIcoon1.style.display = 'none'
-}
-icoon1.onmouseover = verschijn1;
-icoon1.onmouseout = verdwijn1;
+
 
 
